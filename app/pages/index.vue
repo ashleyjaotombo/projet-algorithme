a9804b18d0ba3246ae4a586e152cfc9d0f3f0806
Ashley <ashh@MacBookAir.lan>
2025-10-11 20:54:00 +0200

    homepage

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,7 +5,7 @@ import { DFS } from '~/data/DFS';
 BFS("Caen");
 DFS("Caen");
 
-         // <v-list :items="items"></v-list>
+
 
 
 const items = [
@@ -30,25 +30,30 @@ const items = [
 
 <template>
   <v-container>
-    <h1 class="font-bold text-center text-red">PROJET - ALGORITHME ET THÉORIES DES GRAPHES</h1>
+    <h2 class="text-center text-red">PROJET - ALGORITHME ET THÉORIES DES GRAPHES</h2>
+    <p class="font-weight-medium text-center">Ce site vous permettra de visualiser différents algorithmes applicables à un exemple vu en cours.</p>
     <v-row class="" height="600">
       <v-col cols="6">
-        <v-img src="/graphe.jpg" width="900" height="500"/>
+        <v-img src="/graphe.png" width="900" height="500"/>
       </v-col>
       <v-col cols="6" class="d-flex align-center justify-center">
+        <v-col>
+          <p class="text-weight-regular mb-6">Vous pouvez tester chacune des 4 opérations disponibles ci-dessous : </p>
         <v-card
           class="mx-auto rounded-xl mt-200"
           max-width="500" max-height="240"
         >
-          <v-list density="compact">
-            <v-list-item v-for="(item, index) in items" :key="index" :value="item" color="primary"> 
-              <NuxtLink :to="`/${item.value}/`"  class="text-decoration-none text-white">
-                <v-list-item-title>{{item.title}}</v-list-item-title>
+          <v-list density="compact" class="bg-white">
+            <v-list-item v-for="(item, index) in items" :key="index" :value="item" color="primary">
+              <NuxtLink :to="`/${item.value}/`"  class="text-decoration-none text-black">
+                <v-list-item-title><v-icon size="small" color="primary">mdi-chevron-right</v-icon>{{item.title}}</v-list-item-title>
               </NuxtLink>
             </v-list-item>
           
           </v-list>
         </v-card>
+        </v-col>
+        
       </v-col>
     </v-row>
   </v-container>
@@ -57,3 +62,4 @@ const items = [
   
 
 </template>
+
